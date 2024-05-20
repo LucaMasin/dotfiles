5781b8163a8239308e1cbfa919eb83a91719c5bb
Luca Masin <luca.masin@kuleuven.be>
2024-05-20 12:36:52 +0200

    Added generate github ssh

---
+++ scripts/generate_github_ssh.sh
@@ -0,0 +1,18 @@
+ech "Leave default file path and press enter"
+ssh-keygen -t ed25519 -C "luca.mas93@gmail.com"
+eval "$(ssh-agent -s)"
+ssh-add ~/.ssh/id_ed25519
+
+echo "Public key for GitHub:"
+cat ~/.ssh/id_ed25519.pub
+
+echo
+echo "Public key for GitHub copied to clipboard"
+if command -v clip.exe &> /dev/null
+then
+    CLIPCMD=clip.exe
+else
+    CLIPCMD=clip
+fi
+
+$CLIPCMD < ~/.ssh/id_ed25519.pub
