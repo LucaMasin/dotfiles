18bbcf788ce3421d9785549fe2e4da336aa18df5
Luca Masin <luca.masin@kuleuven.be>
2024-06-02 11:48:11 +0200

    chore: Add neotest and neotest-python plugins to nvim config

---
+++ .config/nvim/lua/plugins/neotest.lua
@@ -0,0 +1,68 @@
+local neotest = {
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-neotest/neotest-plenary",
+    "nvim-neotest/neotest-vim-test",
+    "nvim-lua/plenary.nvim",
+    "antoinemadec/FixCursorHold.nvim",
+    "nvim-treesitter/nvim-treesitter"
+  },
+    config = function()
+    require("neotest").setup({
+        adapters = {
+            require("neotest-python")({
+                dap = { justMyCode = false },
+            }),
+            require("neotest-plenary"),
+            require("neotest-vim-test")({
+                ignore_file_types = { "python", "vim", "lua" },
+            }),
+            require("neodev").setup({
+                library = { plugins = { "neotest" }, types = true },
+            })
+        }
+    })
+    end,
+
+}
+
+local neotest_python = {
+  "nvim-neotest/neotest-python",
+  config = function()
+		require("neotest").setup({
+        adapters = {
+            require("neotest-python")({
+                -- Extra arguments for nvim-dap configuration
+                -- See https://github.com/microsoft/debugpy/wiki/Debug-configuration-settings for values
+                dap = { justMyCode = false },
+                -- Command line arguments for runner
+                -- Can also be a function to return dynamic values
+                args = {"--log-level", "DEBUG"},
+                -- Runner to use. Will use pytest if available by default.
+                -- Can be a function to return dynamic value.
+                runner = "pytest",
+                -- Custom python path for the runner.
+                -- Can be a string or a list of strings.
+                -- Can also be a function to return dynamic value.
+                -- If not provided, the path will be inferred by checking for 
+                -- virtual envs in the local directory and for Pipenev/Poetry configs
+                python = "/python",
+                -- Returns if a given file path is a test file.
+                -- NB: This function is called a lot so don't perform any heavy tasks within it.
+                is_test_file = function(file_path)
+                    return file_path:match("test_.*%.py$") ~= nil
+                end,
+                -- !!EXPERIMENTAL!! Enable shelling out to `pytest` to discover test
+                -- instances for files containing a parametrize mark (default: false)
+                pytest_discover_instances = true,
+            })
+        }
+		})
+	end,
+}
+
+return {
+  neotest,
+  neotest_python
+}
