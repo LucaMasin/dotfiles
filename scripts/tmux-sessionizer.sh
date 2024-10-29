ed4c8ac70d3ed502362567f661b5d6226b8b33cc
Luca Masin <luca.masin@kuleuven.be>
2024-10-29 22:57:15 +0100

    Refactor: Update tmux-sessionizer.sh script to include symbolic links in directory search

--- scripts/tmux-sessionizer.sh
+++ scripts/tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/python ~/docker ~/scripts ~/data ~/scratch -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/python ~/docker ~/scripts ~/data ~/scratch -mindepth 1 -maxdepth 1 \( -type d -o -type l \) | fzf)
 fi
 
 if [[ -z $selected ]]; then
