461764f58d702e03b2ba164aa97ca18b0bd9b10d
Luca Masin <luca.masin@kuleuven.be>
2025-01-17 19:51:52 +0100

    Refactor: Update LSP configuration to include ruff and comment out pyright

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -172,7 +172,8 @@ return { -- LSP Configuration & Plugins
 				},
 			},
 
-			pyright = {},
+			ruff = {},
+			-- pyright = {},
 			-- rust_analyzer = {},
 			-- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
 			--
