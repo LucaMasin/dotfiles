5bccf0f8daf0f63c9f57b6c84ff103924f06c4af
Luca Masin <luca.masin@kuleuven.be>
2025-03-02 00:15:04 +0100

    Refactor: Add net-tools to shell essentials installation in Ubuntu setup script

--- setup_scripts/auto_setup/ubuntu/shell.sh
+++ setup_scripts/auto_setup/ubuntu/shell.sh
@@ -2,6 +2,6 @@
 
 echo "Installing shell essentials"
 
-sudo apt install zsh fzf ripgrep git btop -y
+sudo apt install zsh fzf ripgrep git btop net-tools -y
 curl -sS https://starship.rs/install.sh | sh
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
