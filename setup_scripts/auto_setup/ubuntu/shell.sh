17a11f9ed607ba13a0c69933f1c306df0d7aeaae
Luca Masin <luca.masin@kuleuven.be>
2025-03-02 13:40:07 +0100

    Refactor: Update shell installation script to use pre-confirmation for Starship installation

--- setup_scripts/auto_setup/ubuntu/shell.sh
+++ setup_scripts/auto_setup/ubuntu/shell.sh
@@ -3,5 +3,6 @@
 echo "Installing shell essentials"
 
 sudo apt install zsh fzf ripgrep git btop net-tools -y
-curl -sS https://starship.rs/install.sh | sh
+# curl -sS https://starship.rs/install.sh | sh
+STARSHIP_PRECONFIRM=yes sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
