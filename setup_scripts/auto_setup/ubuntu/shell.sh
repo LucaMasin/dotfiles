d049758be1c469e050522ad68298bdd8fbcf834d
Luca Masin <luca.masin@kuleuven.be>
2025-04-16 16:13:21 +0200

    Add Rust and yazi installation to shell setup script

--- setup_scripts/auto_setup/ubuntu/shell.sh
+++ setup_scripts/auto_setup/ubuntu/shell.sh
@@ -5,3 +5,10 @@ echo "Installing shell essentials"
 sudo apt install zsh fzf ripgrep git btop net-tools -y
 curl -sS https://starship.rs/install.sh | sh
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
+
+# Install rust and yazi
+curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
+. "$HOME/.cargo/env"
+
+cargo install --locked yazi-fm yazi-cli
+cargo install tokei
