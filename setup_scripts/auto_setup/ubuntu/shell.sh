fe03cdfd22104e7a9d7dd0e032f969abde47df09
Luca Masin <luca.masin@kuleuven.be>
2025-04-21 15:03:15 +0200

    Enhance shell setup script to include pipx and install development tools like uv and poetry, while ensuring .zshrc is created for shell completions.

--- setup_scripts/auto_setup/ubuntu/shell.sh
+++ setup_scripts/auto_setup/ubuntu/shell.sh
@@ -2,7 +2,9 @@
 
 echo "Installing shell essentials"
 
-sudo apt install zsh fzf ripgrep git btop net-tools -y
+touch $HOME/.zshrc
+
+sudo apt install zsh fzf ripgrep git btop net-tools pipx -y
 curl -sS https://starship.rs/install.sh | sh
 curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 
@@ -12,3 +14,15 @@ curl --proto '=https' --tlsv1.3 https://sh.rustup.rs -sSf | sh
 
 cargo install --locked yazi-fm yazi-cli
 cargo install tokei
+
+# install development tools
+echo "Installing uv"
+curl -LsSf https://astral.sh/uv/install.sh | sh
+echo 'eval "$(uv generate-shell-completion zsh)"' >> ~/.zshrc
+echo 'eval "$(uvx --generate-shell-completion zsh)"' >> ~/.zshrc
+
+echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
+echo 'eval "$(uvx --generate-shell-completion bash)"' >> ~/.bashrc
+
+echo "Installing poetry"
+pipx install poetry
